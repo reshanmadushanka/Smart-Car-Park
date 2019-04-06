0176a69d4a08866fca6522a99cd7893d7b928bd9
umanadee <49117767+umanadee@users.noreply.github.com>
2019-04-06 08:34:35 +0530

    morning

--- parking-data.php
+++ parking-data.php
@@ -1,7 +1,7 @@
 <?php 
 include './database/config.php';   
 $sql = $db->prepare("SELECT * FROM tbl_slot"); //get data from database
-$sql->execute();
-$data = $sql->fetchAll(); 
-echo json_encode($data);
+$sql->execute(); //run the query
+$data = $sql->fetchAll(); //make a array in php database table data
+echo json_encode($data); //json encode the data to read in javascript
 ?>

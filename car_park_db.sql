686bb0b80dfcd44612b4d2e02cd9580280c35672
umanadee <49117767+umanadee@users.noreply.github.com>
2019-04-06 10:29:22 +0530

    Add Interim Database

---
+++ car_park_db.sql
@@ -0,0 +1,243 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 06, 2019 at 06:58 AM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 7.2.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `car_park_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_booking`
+--
+
+CREATE TABLE `tbl_booking` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `book_date` date NOT NULL,
+  `from` time NOT NULL,
+  `to` time NOT NULL,
+  `slot_id` int(11) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_booking`
+--
+
+INSERT INTO `tbl_booking` (`id`, `user_id`, `book_date`, `from`, `to`, `slot_id`, `timestamp`) VALUES
+(1, 1, '2019-03-26', '11:00:00', '11:30:00', 1, '2019-03-26 18:20:44'),
+(4, 1, '2019-03-26', '10:30:00', '11:00:00', 2, '2019-03-26 18:24:39'),
+(5, 1, '2019-04-26', '00:00:00', '00:00:00', 1, '2019-04-02 18:28:13'),
+(6, 1, '2019-04-26', '00:00:00', '00:00:00', 1, '2019-04-02 18:28:57'),
+(7, 1, '2019-04-26', '10:00:00', '11:00:00', 3, '2019-04-02 18:29:50'),
+(8, 1, '2019-04-26', '10:00:00', '11:00:00', 1, '2019-04-02 18:30:08'),
+(9, 1, '2019-04-26', '10:00:00', '11:00:00', 4, '2019-04-02 18:35:16');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_payment`
+--
+
+CREATE TABLE `tbl_payment` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_role`
+--
+
+CREATE TABLE `tbl_role` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_role`
+--
+
+INSERT INTO `tbl_role` (`id`, `name`, `timestamp`) VALUES
+(1, 'admin', '2019-03-20 06:48:31'),
+(2, 'customer', '2019-03-20 06:48:31'),
+(3, 'manager', '2019-03-20 06:48:44');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_slider`
+--
+
+CREATE TABLE `tbl_slider` (
+  `id` int(11) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `title` text NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_slider`
+--
+
+INSERT INTO `tbl_slider` (`id`, `image`, `title`, `timestamp`) VALUES
+(1, '2.jpg', 'Car Park ', '2019-04-02 19:14:33'),
+(2, 'camconsults.jpg', 'edwefw', '2019-04-02 19:15:29'),
+(3, '023-automating-video-testing-for-smart-parking-system.jpg', 'Omea', '2019-04-06 03:16:34');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_slot`
+--
+
+CREATE TABLE `tbl_slot` (
+  `id` int(11) NOT NULL,
+  `slot_name` varchar(55) NOT NULL,
+  `status` int(11) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_slot`
+--
+
+INSERT INTO `tbl_slot` (`id`, `slot_name`, `status`, `timestamp`) VALUES
+(1, 'slot 1', 0, '2019-03-09 18:16:57'),
+(2, 'slot 2', 1, '2019-03-09 18:16:57'),
+(3, 'slot3', 1, '2019-04-01 16:20:41'),
+(4, 'slot4', 0, '2019-04-01 16:20:41');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id` int(11) NOT NULL,
+  `f_name` varchar(200) NOT NULL,
+  `l_name` varchar(200) NOT NULL,
+  `NIC` varchar(20) NOT NULL,
+  `mobile` varchar(22) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `role_id` int(11) UNSIGNED NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id`, `f_name`, `l_name`, `NIC`, `mobile`, `city`, `email`, `password`, `role_id`, `timestamp`) VALUES
+(2, 'admin', 'car', '912455663v', '0714852233', 'galle', 'admin@gmail.com', '$2y$10$CYFlVw3l8zCLgcROdWQQcOmqfpqcfgYeivUVMqG9CMbpULUKiXFRK', 1, '2019-03-20 07:15:06');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_booking`
+--
+ALTER TABLE `tbl_booking`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_payment`
+--
+ALTER TABLE `tbl_payment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_role`
+--
+ALTER TABLE `tbl_role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_slider`
+--
+ALTER TABLE `tbl_slider`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_slot`
+--
+ALTER TABLE `tbl_slot`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `role_id` (`role_id`),
+  ADD KEY `role_id_2` (`role_id`),
+  ADD KEY `role_id_3` (`role_id`),
+  ADD KEY `role_id_4` (`role_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_booking`
+--
+ALTER TABLE `tbl_booking`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `tbl_payment`
+--
+ALTER TABLE `tbl_payment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_role`
+--
+ALTER TABLE `tbl_role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tbl_slider`
+--
+ALTER TABLE `tbl_slider`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tbl_slot`
+--
+ALTER TABLE `tbl_slot`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
